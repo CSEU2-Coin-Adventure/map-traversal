d71ce79c5dc446169209c31d82e82652df7d0220
mattshardman <mattshardman@gmail.com>
2019-11-20 12:03:46 +0800

    add cli

--- graph.py
+++ graph.py
@@ -1,7 +1,9 @@
 import json
 import time
 import requests
-
+import csv
+import sys
+from termcolor import colored
 
 class Queue:
     def __init__(self):
@@ -65,16 +67,23 @@ class Mover:
         self.graph = None
         self.path = []
         self.directions = []
-        self.rooms = []
+        self.rooms = {}
         self.current_room = None
 
     def _set_current_room(self):
         room = requests.get("https://lambda-treasure-hunt.herokuapp.com/api/adv/init/", headers={
             'Authorization': 'Token 91eab72c1255c3828263a3a60a6cefc409f6461c'}).json()
         self.current_room = room
-        print("Current room: ", self.current_room)
+        print("-------------------------")
+        print("You've in: ", room.get('room_id'))
+        print("It's a: ", room.get('title'))
+        print("About: ", room.get('description'))
+        print("Additional info: ", room.get('messages'))
+        print("Current cool down: ", room.get('cooldown'))
+        print("-------------------------")
+        time.sleep(room.get('cooldown'))
 
-    def _get_rooms(self):
+    def _set_rooms(self):
         pass
 
     def init(self):
@@ -89,24 +98,92 @@ class Mover:
             new_room = requests.post("https://lambda-treasure-hunt.herokuapp.com/api/adv/move/", json={
                 'direction': d}, headers={'Authorization': 'Token 91eab72c1255c3828263a3a60a6cefc409f6461c'}).json()
             print(new_room)
+            print("-------------------------")
+            print("You've entered room: ", new_room.get('room_id'))
+            print("It's a: ", new_room.get('title'))
+            print("About: ", new_room.get('description'))
+            print("Additional info: ", new_room.get('messages'))
+            print("Current cool down: ", new_room.get('cooldown'))
+            print("-------------------------")
             time.sleep(new_room.get('cooldown'))
 
     def hunt_treasure(self):
         pass
 
     def go_home(self):
-        (directions, path) = self.graph.dft(self.current_room.get('room_id'), 0)
+        (directions, path) = self.graph.dft(
+            self.current_room.get('room_id'), 0)
         self.directions = directions
         self.path = [str(num) for num in path]
         print("Directions: ", self.directions)
         print("Path", self.path)
-    
+
     def go_to_shop(self):
-        (directions, path) = self.graph.dft(self.current_room.get('room_id'), 1)
+        (directions, path) = self.graph.dft(
+            self.current_room.get('room_id'), 1)
+        self.directions = directions
         self.path = [str(num) for num in path]
         print("Directions: ", self.directions)
         print("Path", self.path)
+        self.go()
 
-m = Mover()
-m.init()
-m.go_home()
+    def go_to_shrine(self):
+        (directions, path) = self.graph.dft(
+            self.current_room.get('room_id'), 461)
+        self.directions = directions
+        self.path = [str(num) for num in path]
+        print("Directions: ", self.directions)
+        print("Path", self.path)
+        self.go()
+
+    def pray(self):
+        if self.current_room == 461:
+            pass
+        else:
+            print("You don't seem to be at the shrine")
+
+def print_instructions():
+    print("You need to enter a command.")
+    print("Enter one of the following")
+    print("     - home - this will take you back to 0 from your current location")
+    print("     - shop - this will take you to the shop from your current location")
+    print("     - shrine - this will take you to the shop from your current location")
+
+
+def start(inputs):
+    m = Mover()
+    m.init()
+    print(inputs)
+    if len(inputs) < 2:
+        print_instructions()
+    elif inputs[1] == "home":
+        text = input("Are you sure you want to go home? [y or n]\n")
+        if text == "y":
+            print("Going home")
+            m.go_home()
+        elif text == "n":
+            print("Ok")
+        else:
+            print("enter y or n")
+    elif inputs[1] == "shop":
+        text = input("Are you sure you want to go to the shop? [y or n]\n")
+        if text == "y":
+            print("Going to the shop")
+            m.go_to_shop()
+        elif text == "n":
+            print("Ok")
+        else:
+            print("enter y or n")
+    elif inputs[1] == "shrine":
+        text = input("Are you sure you want to go to the shop? [y or n]\n")
+        if text == "y":
+            print("Going to the shrine")
+            m.go_to_shrine()
+        elif text == "n":
+            print("Ok")
+        else:
+            print("enter y or n")
+    else:
+        print_instructions()
+
+start(sys.argv)
